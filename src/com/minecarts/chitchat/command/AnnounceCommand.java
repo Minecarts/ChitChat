53529a87bf4c6cba9977c644e77d939e3fc4d8f6
Stephen <stephen@jazer.com>
2012-02-26 03:14:51 -0500

    Announce from console fixed

--- src/com/minecarts/chitchat/command/AnnounceCommand.java
+++ src/com/minecarts/chitchat/command/AnnounceCommand.java
@@ -9,6 +9,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class AnnounceCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -16,7 +17,7 @@ public class AnnounceCommand implements CommandExecutor {
         
         String message = StringHelper.join(args, 0);
         ChannelLink link = ChannelManager.getOrCreateChannelLink("Announcement");
-        link.relayMessage(ChatColor.YELLOW + message);
+        link.relayMessage(new Player[]{},ChatColor.YELLOW + message);
         return true;
     }
 }
