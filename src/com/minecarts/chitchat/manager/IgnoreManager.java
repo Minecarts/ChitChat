2ea65891435bbe788341622c95e36e046530efad
Stephen <stephen@jazer.com>
2012-03-07 20:29:11 -0500

    Ignore list no longer has duplicate names (Fixes #37)

--- src/com/minecarts/chitchat/manager/IgnoreManager.java
+++ src/com/minecarts/chitchat/manager/IgnoreManager.java
@@ -4,33 +4,30 @@ import com.minecarts.chitchat.ChitChat;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.util.Map;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class IgnoreManager {
     
-    private static Map<Player, List<String>> ignoreList = new HashMap<Player, List<String>>();
+    private static Map<Player, Set<String>> ignoreList = new HashMap<Player, Set<String>>();
     
     public static Boolean isIgnoring(Player player, String ignoreeName){
-        List<String> ignoredPlayers = ignoreList.get(player);
+        Set<String> ignoredPlayers = ignoreList.get(player);
         if(ignoredPlayers == null) return false;
         return ignoredPlayers.contains(ignoreeName);
     }
 
     //The following functions support strings to load them from the database (thus loading offline players)
     public static void ignorePlayer(Player player, String ignoreeName){
-        List<String> ignoredPlayers = ignoreList.get(player);
+        Set<String> ignoredPlayers = ignoreList.get(player);
         if(ignoredPlayers == null){
-            ignoredPlayers = new ArrayList<String>();
+            ignoredPlayers = new HashSet<String>();
             ignoreList.put(player, ignoredPlayers);
         }
         ignoredPlayers.add(ignoreeName);
     }
 
     public static void unignorePlayer(Player player, String ignoreeName){
-        List<String> ignoredPlayers = ignoreList.get(player);
+        Set<String> ignoredPlayers = ignoreList.get(player);
         if(ignoredPlayers == null) return;
         ignoredPlayers.remove(ignoreeName);
         if(ignoredPlayers.isEmpty()) ignoreList.remove(player);
@@ -41,7 +38,7 @@ public class IgnoreManager {
     }
     
     public static String[] getIgnoreList(Player player) {
-        List<String> list = ignoreList.get(player);
+        Set<String> list = ignoreList.get(player);
         return list == null ? new String[0] : list.toArray(new String[0]);
     }
     
