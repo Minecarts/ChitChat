0b330ad8c825360b6620b7ccc1e2f54a29cb0d95
Kevin Ingersoll <kingersoll@gmail.com>
2012-06-05 15:15:28 -0700

    Improve mute handling, fixes #48

--- src/com/minecarts/chitchat/manager/MuteManager.java
+++ src/com/minecarts/chitchat/manager/MuteManager.java
@@ -4,14 +4,19 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Date;
 
-import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.permissions.Permissible;
+
 
 public class MuteManager {
-    private static Map<Player, Date> mutedPlayers = new HashMap<Player, Date>();
+    private static Map<String, Date> mutedPlayers = new HashMap<String, Date>();
     
-    public static Boolean isMuted(Player player) {
-        if(player.hasPermission("chitchat.mute")) return true;
-        
+    public static Boolean isMuted(OfflinePlayer player) {
+        if(player instanceof Permissible && ((Permissible) player).hasPermission("chitchat.mute")) return true;
+        return isMuted(player.getName());
+    }
+    
+    public static Boolean isMuted(String player) {
         Date expires = mutedPlayers.get(player);
         if(expires == null) return false;
         
@@ -24,17 +29,26 @@ public class MuteManager {
         }
     }
     
-    public static void mute(Player player) {
+    public static void mute(OfflinePlayer player) {
+        mute(player.getName());
+    }
+    public static void mute(String player) {
         mute(player, 1000 * 60 * 15);
     }
     
-    public static void mute(Player player, long duration) {
+    public static void mute(OfflinePlayer player, long duration) {
+        mute(player.getName(), duration);
+    }
+    public static void mute(String player, long duration) {
         if(duration > 0) {
             mutedPlayers.put(player, new Date(new Date().getTime() + duration));
         }
     }
     
-    public static void unmute(Player player) {
+    public static void unmute(OfflinePlayer player) {
+        unmute(player.getName());
+    }
+    public static void unmute(String player) {
         mutedPlayers.remove(player);
     }
 }
