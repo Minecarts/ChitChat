acce760432d529b7c4086a13d5eddd57139ae36b
Kevin Ingersoll <kingersoll@gmail.com>
2012-06-05 20:52:40 -0700

    Should have used OfflinePlayers from the start! Built in goodies :)

--- src/com/minecarts/chitchat/manager/MuteManager.java
+++ src/com/minecarts/chitchat/manager/MuteManager.java
@@ -9,14 +9,12 @@ import org.bukkit.permissions.Permissible;
 
 
 public class MuteManager {
-    private static Map<String, Date> mutedPlayers = new HashMap<String, Date>();
+    private static Map<OfflinePlayer, Date> mutedPlayers = new HashMap<OfflinePlayer, Date>();
+    
     
     public static Boolean isMuted(OfflinePlayer player) {
         if(player instanceof Permissible && ((Permissible) player).hasPermission("chitchat.mute")) return true;
-        return isMuted(player.getName());
-    }
-    
-    public static Boolean isMuted(String player) {
+        
         Date expires = mutedPlayers.get(player);
         if(expires == null) return false;
         
@@ -30,25 +28,16 @@ public class MuteManager {
     }
     
     public static void mute(OfflinePlayer player) {
-        mute(player.getName());
-    }
-    public static void mute(String player) {
         mute(player, 1000 * 60 * 15);
     }
     
     public static void mute(OfflinePlayer player, long duration) {
-        mute(player.getName(), duration);
-    }
-    public static void mute(String player, long duration) {
         if(duration > 0) {
             mutedPlayers.put(player, new Date(new Date().getTime() + duration));
         }
     }
     
     public static void unmute(OfflinePlayer player) {
-        unmute(player.getName());
-    }
-    public static void unmute(String player) {
         mutedPlayers.remove(player);
     }
 }
