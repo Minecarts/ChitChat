8cdb854c4b36f7c1b3fc61e7225302dae9a01153
Kevin Ingersoll <kingersoll@gmail.com>
2012-06-20 17:40:23 -0700

    Use strings instead of offline players, (again) fixes #48

--- src/com/minecarts/chitchat/manager/MuteManager.java
+++ src/com/minecarts/chitchat/manager/MuteManager.java
@@ -9,20 +9,20 @@ import org.bukkit.permissions.Permissible;
 
 
 public class MuteManager {
-    private static Map<OfflinePlayer, Date> mutedPlayers = new HashMap<OfflinePlayer, Date>();
+    private static Map<String, Date> mutedPlayers = new HashMap<String, Date>();
     
     
     public static Boolean isMuted(OfflinePlayer player) {
         if(player instanceof Permissible && ((Permissible) player).hasPermission("chitchat.mute")) return true;
         
-        Date expires = mutedPlayers.get(player);
+        Date expires = mutedPlayers.get(player.getName().toLowerCase());
         if(expires == null) return false;
         
         if(expires.after(new Date())) {
             return true;
         }
         else {
-            mutedPlayers.remove(player);
+            mutedPlayers.remove(player.getName().toLowerCase());
             return false;
         }
     }
@@ -33,11 +33,11 @@ public class MuteManager {
     
     public static void mute(OfflinePlayer player, long duration) {
         if(duration > 0) {
-            mutedPlayers.put(player, new Date(new Date().getTime() + duration));
+            mutedPlayers.put(player.getName().toLowerCase(), new Date(new Date().getTime() + duration));
         }
     }
     
     public static void unmute(OfflinePlayer player) {
-        mutedPlayers.remove(player);
+        mutedPlayers.remove(player.getName().toLowerCase());
     }
 }
